46caa29e26069cc09b72a34d64e98d90bd7f7811
vibrixbeat-svgserz <vibrixbeat@gmail.com>
2026-09-05 15:11:07 +0300

    Change static file serving directory to current directory

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(__dirname));
 
 const db = {
   users: {},
