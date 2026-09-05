c9115569de8d16d7b17a286ea62e0f64b3a690ad
vibrixbeat-svgserz <vibrixbeat@gmail.com>
2026-09-05 15:40:37 +0300

    Change path to index.html in sendFile method

--- index.js
+++ index.js
@@ -445,7 +445,7 @@ app.post('/api/admin/withdraws/:id', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => console.log('Star of Olimp on port', PORT));
